95b5542b90d7ed6b2a1def036ee686362842e9dd
Davit Ghushchyan <7693799@gmail.com>
2025-05-05 22:54:21 +0400

    Project Initial commit

---
+++ MarktTests/MarktTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarktTests.swift
+//  MarktTests
+//
+//  Created by Davit Ghushchyan on 05.05.25.
+//
+
+import Testing
+@testable import Markt
+
+struct MarktTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
